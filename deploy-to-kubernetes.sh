982ceead054187ca8e929a6877fc9e410fb6b266
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-12-16 19:56:58 +0600

    bug fix

--- deploy-to-kubernetes.sh
+++ deploy-to-kubernetes.sh
@@ -8,3 +8,5 @@ kubectl apply -f kubernetes/app-deployment.yaml
 kubectl apply -f kubernetes/app-service.yaml
 
 kubectl apply -f kubernetes/ingress.yaml
+
+kubectl exec -it $(kubectl get pods | grep '^express' | head -n 1 | awk '{print $1}') -- sh -c "npx prisma migrate dev && npx prisma db push"
